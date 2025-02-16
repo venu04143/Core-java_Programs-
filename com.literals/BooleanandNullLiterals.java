676f62c4be9c6397cdc50c84bd4ac2195c033e63
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-02-16 20:12:50 +0530

    Add files via upload

---
+++ com.literals/BooleanandNullLiterals.java
@@ -0,0 +1,24 @@
+package com.literals;
+
+public class BooleanandNullLiterals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// null is  a literal to keep the object default values
+		BooleanandNullLiterals B =null;
+		
+		String s=null;
+		
+		// int i =null;  we can assign null to only objects
+		
+		// we can't assign null to primitive data types
+		
+		// true ,false ,null may seem like keywords but they are literals
+		
+		boolean b1 =true;
+		boolean b2 = false;
+		
+	}
+
+}
