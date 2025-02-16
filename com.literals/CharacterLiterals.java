a9f282868ee6b4d62f9a24aba514eb513162a23d
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-02-16 20:15:18 +0530

    Add files via upload

---
+++ com.literals/CharacterLiterals.java
@@ -0,0 +1,22 @@
+package com.literals;
+
+public class CharacterLiterals {
+
+	public static void main(String[] args) {
+		
+		// char maximum range 0 to 65535
+		
+		char c ='K';    // single character in single quote
+		char c1 = 66;   // ASCII code 
+		char c2 = '\u0040';  // Uni-code
+		
+		//char c3 ='Ke'; Invalid
+		
+		System.out.println(c);
+		System.out.println(c1);
+		System.out.println(c2);
+		
+
+	}
+
+}
