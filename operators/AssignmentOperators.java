2190a6bb98183a57f5e3390e68778bc62b5fa50d
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-02-18 22:17:41 +0530

    Add files via upload

---
+++ operators/AssignmentOperators.java
@@ -0,0 +1,53 @@
+package com.operators;
+
+public class AssignmentOperators {
+
+	public static void main(String[] args) {
+		
+		
+		int a = 100; 
+		int b =50;
+		
+		System.out.println(a);
+		
+		System.out.println(b);
+		
+		int sum;
+		sum=5;
+		
+	 
+		sum +=4.5 ;        // compound operator
+		
+		System.out.println(sum);
+//		
+//		sum+=b;
+//		
+//		System.out.println(sum);
+//		
+//		
+//		
+//		sum -=a ;
+//		
+//		System.out.println(sum);
+//		
+//		a -=b;
+//		System.out.println(a);
+//		
+//		a *= b;
+//		
+//		System.out.println(a);
+//		
+//		sum *= a+b;
+//		System.out.println(sum);
+//		
+//		sum /= a+b;
+//		System.out.println(sum);
+//		
+//		sum %= a;
+//		
+//		System.out.println(sum);
+//		
+
+	}
+
+}
