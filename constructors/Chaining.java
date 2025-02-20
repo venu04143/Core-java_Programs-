42018112bd9c60d5184705f2abb080cfdf07d125
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-02-20 21:20:31 +0530

    Add files via upload

---
+++ constructors/Chaining.java
@@ -0,0 +1,67 @@
+package com.constructors;
+
+public class Chaining {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+//class Person{
+//	
+//	String name;
+//    int age;
+//    
+//    Person(){
+//    	this("Venu");
+//    	System.out.println("no arg constructor called from Person class..");
+//    }
+//    Person(String name){
+//    	this("Venu",23);
+//    	this.name =name;
+//    	System.out.println("parameterize 1 called from Person class..");
+//    	
+//    }
+//    Person(String name, int age){
+//    	this.name=name;
+//    	this.age=age;
+//    	
+//    }
+//    void display() {
+//    	System.out.println("Person name : "+name);
+//    	System.out.println("Person age : "+age);
+//    }
+//    public static void main(String[] args) {
+//		
+//    	System.out.println("Person main method called...");
+//    	
+////    	Person p1= new Person();
+////    	p1.display();
+//	}
+//
+//}
+//class Doctor extends Person{
+//	int age;
+//	int name;
+//	
+//	Doctor(){
+//		super();
+//	}
+//	
+//	Doctor(String name){
+//		super();
+//	}
+//	Doctor(String name , int age){
+//		super();
+//	}
+//	void display1() {
+//		System.out.println("Doctor name  :"+ name);
+//		System.out.println("Doctor age  :"+ age);
+//	}
+//	public static void main(String[] args) {
+//		System.out.println("Doctor main method called..");
+//		
+//		Doctor d1 = new Doctor();
+//	}
+//}
