cd250bbbd1d7f44b2d83c67ae2e2624707197151
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-03-06 23:15:07 +0530

    Add files via upload

---
+++ loopingstatements/For01.java
@@ -0,0 +1,20 @@
+package com.loopingstatements;
+
+import java.util.Scanner;
+
+public class For01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a N value :");
+		int n= sc.nextInt();
+		
+		for(int i= 0;i<=10;i++) {
+			System.out.println(n +" x "+i+" = " +(n*i));
+		}
+
+	}
+
+}
