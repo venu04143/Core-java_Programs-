0182b1b9f3d7691b8e4a2cf5baf9bbb95c044451
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-03-06 23:11:31 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,32 @@
+package com.loopingstatements;
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 0;
+		while (i != 10) {
+			System.out.print(i);
+			i++;
+		}
+		
+		System.out.println();
+		
+		
+		int j=10;
+		while(j!=0) {
+			System.out.print(j);
+			j--;
+		}
+		System.out.println();
+		
+	  int k =10;
+	  do {
+		  System.out.println(k--);
+	  }
+		while(k!=0);	
+	
+
+   }
+}
