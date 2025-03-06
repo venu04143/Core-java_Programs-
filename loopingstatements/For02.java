5b91ec93d039d3df4bbcdb82a8d7a54e9acb7a3e
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-03-06 23:15:52 +0530

    Add files via upload

---
+++ loopingstatements/For02.java
@@ -0,0 +1,18 @@
+package com.loopingstatements;
+
+public class For02 {
+
+	public static void main(String[] args) {
+	    
+		
+		for(int i=0,j=10;i<=10 && j>=0;i++ ,j--) {
+
+			System.out.print(i+"\t");
+			
+			System.out.print(j+"\t");
+			
+		}
+
+	}
+
+}
