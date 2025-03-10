cefef6248d0b13212889a5e14e27066ef07c0e05
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-03-10 22:31:08 +0530

    Add files via upload

---
+++ UsingLong.java
@@ -0,0 +1,18 @@
+package com.assignmentsloopingstatements;
+
+public class UsingLong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(long l=0L;l<10L;l++) {
+			System.out.println(l);
+		}
+		
+		for (long l = 9223372036854775800L; l < 9223372036854775807L; l++) {
+		    System.out.println(l);
+		}
+		
+	}
+
+}
