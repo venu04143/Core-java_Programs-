9e9ec77a47ca1912d0dba1f2b92826e7e2448d40
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-03-10 22:29:22 +0530

    Add files via upload

---
+++ assignmentsloopingstatements/UsingDouble.java
@@ -0,0 +1,23 @@
+package com.assignmentsloopingstatements;
+
+public class UsingDouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(double d=0;d<10;d++) {
+			System.out.println(d);
+		}
+		
+		for(double d = 0.5;d<5.3;d+=0.25) {
+			System.out.println(d);
+		}
+		
+		
+		for(double d =100000000000d;d<100000000010d;d++) {
+			System.out.println(d);
+		}
+
+	}
+
+}
