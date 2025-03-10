c151fdaffeb5afc05efb51e6e9eae3d8f680c6bf
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-03-10 22:29:58 +0530

    Add files via upload

---
+++ assignmentsloopingstatements/UsingFloat.java
@@ -0,0 +1,29 @@
+package com.assignmentsloopingstatements;
+
+public class UsingFloat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		for(float f =0.5f;f<5F;f++) {
+			System.out.println(f);
+		}
+		
+		
+		for(float f=0.23f;f<3.5f;f+=0.5f) {
+			System.out.println(f);
+		}
+		
+		
+		for(float f=0f;f<1.0f;f+=0.1f) {
+			System.out.println(f);
+		}
+		
+		for(float f=0;f<5;f+=0.5) {
+			System.out.println(f);
+		}
+		
+		
+	}
+
+}
