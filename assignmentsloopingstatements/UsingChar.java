580f9de5f93cdd6e2f557dff4978d6a94df5609d
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-03-10 22:28:50 +0530

    Add files via upload

---
+++ assignmentsloopingstatements/UsingChar.java
@@ -0,0 +1,30 @@
+package com.assignmentsloopingstatements;
+
+public class UsingChar {
+	public static void main(String[] args) {
+		
+		for(char a='A';a<='Z';a++) {
+			System.out.print(a+ " ");
+		}
+		
+		System.out.println();
+		
+		for (char c=65;c<99;c++) {
+			System.out.print(c+" ");
+		}
+		
+		System.out.println();
+		
+		for(char c= 0;c<10;c++) {
+			System.out.print(c+ " ");
+		}
+		System.out.println();
+		for(char a='a';a<='z';a++) {
+			System.out.print(a + " ");
+		}
+		
+		
+		
+	}
+
+}
