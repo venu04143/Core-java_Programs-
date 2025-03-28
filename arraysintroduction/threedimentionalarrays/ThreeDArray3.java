796376dbeb286218a784abe90ab035bdaa5989bd
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-03-28 09:08:46 +0530

    Add files via upload

---
+++ arraysintroduction/threedimentionalarrays/ThreeDArray3.java
@@ -0,0 +1,19 @@
+package com.arraysintroduction.threedimentionalarrays;
+
+public class ThreeDArray3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[][][][][][][][][][] arr= new int [2][][][][][][][][][]; // valid
+		
+		
+		// valid
+		
+		int[][][][][][][][][][][][][][][][][][][][][][][][][] arr2= {{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}};
+
+			
+
+	}
+
+}
