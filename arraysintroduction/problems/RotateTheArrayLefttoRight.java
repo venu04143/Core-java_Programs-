debc571be3574844e2f7ed652a17bf099f023120
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-03-29 08:19:44 +0530

    Add files via upload

---
+++ arraysintroduction/problems/RotateTheArrayLefttoRight.java
@@ -0,0 +1,45 @@
+package com.arraysintroduction.problems;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RotateTheArrayLefttoRight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+				
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7 }; // 7 6 5 4 3 2 1
+
+		System.out.println("Enter How Many Time Do You Want To Raotate The Array Left to Right : ");
+		int rotate=sc.nextInt() % arr.length;  // to reduce rotations when Number of rotation is greater than array length
+		
+		System.out.println("Before Rotating The Array is : ");
+		System.out.println(Arrays.toString(arr));
+		System.out.println();
+		rotateArray(arr,rotate);
+		System.out.println("After Rotating "+rotate+" Times The Array is : ");
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+	static void rotateArray(int[] arr, int rotate) {
+		int len = arr.length;
+		reverse(arr, 0, len - 1);
+		
+		reverse(arr, 0, len-1-rotate);
+		reverse(arr, (len-1-rotate+1), len - 1);
+
+	}
+
+	static void reverse(int[] arr, int start, int end) {
+		while (start <= end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}
+
+}
