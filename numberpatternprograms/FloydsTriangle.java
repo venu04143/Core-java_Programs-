109a779124d8d70ef575820d2131bdc7f7d966ae
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-04-11 08:40:52 +0530

    Add files via upload

---
+++ numberpatternprograms/FloydsTriangle.java
@@ -0,0 +1,21 @@
+package com.numberpatternprograms;
+
+public class FloydsTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=5;
+		int num=1;
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(num+" ");
+				num++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
