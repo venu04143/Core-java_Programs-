48df5fec2a1dfccebde88511420023fc3c77a1c0
Ontipuli Venu Gopal <150226192+venu04143@users.noreply.github.com>
2025-04-15 07:26:23 +0530

    Add files via upload

---
+++ Rivision/src/com/rivision/Rivision4.java
@@ -0,0 +1,16 @@
+package com.rivision;
+
+public class Rivision4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+		for(int i=0;i<arr.length;i++) {
+			for(i=0;i<arr.length;i++) {
+				System.out.println(arr[1][1]);
+			}
+		}
+
+	}
+
+}
